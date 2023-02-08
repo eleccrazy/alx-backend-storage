3ad08066fbd9036a58d827f9c0bc28b85b8a9271
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-08 21:33:55 +0300

    Capitalize sql statements

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
 
-SELECT origin, sum(fans) as nb_fans
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin ORDER BY nb_fans DESC;
