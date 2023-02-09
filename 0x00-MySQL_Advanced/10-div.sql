a89a03ec8985e53734f8d46678a652b1f01646db
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-09 19:55:05 +0300

    Drop added on the top

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,6 +1,6 @@
 -- A SQL Script that creates a function SafeDiv that divides (and returns) the first by
---the second number or returns 0 if the second number is equal to 0.
-
+-- the second number or returns 0 if the second number is equal to 0.
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
