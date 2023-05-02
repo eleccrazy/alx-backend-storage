6965047fc903abf8e9ce1b7d196fe0cbc5739553
Gizachew Bayness <eleccrazy24@gmail.com>
2023-05-02 15:44:58 +0300

    Single document inserting

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,5 +3,5 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-    """ Inserts a new document in a collection based on kwargs """
-    return mongo_collection.insert(kwargs)
+    """Inserts a new document to the collection"""
+    return mongo_collection.insert_one(kwargs).inserted_id
