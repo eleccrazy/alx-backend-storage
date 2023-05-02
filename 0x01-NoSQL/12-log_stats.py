e9208d202ad28ad36c27098b9cd7b3d0c060304c
Gizachew Bayness <eleccrazy24@gmail.com>
2023-05-02 21:16:32 +0300

    Fix spelling in output

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,18 +4,19 @@ from pymongo import MongoClient
 
 if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_db = client.logs.nginx
+    nginx_col = client.logs.nginx
 
-    count_docs = len(list(nginx_db.find()))
-    count_get = nginx_db.count_documents({'method': 'GET'})
-    count_post = nginx_db.count_documents({'method': 'POST'})
-    count_put = nginx_db.count_documents({'method': 'PUT'})
-    count_patch = nginx_db.count_documents({'method': 'PATCH'})
-    count_delete = nginx_db.count_documents({'method': 'DELETE'})
-    count_status_path = nginx_db.count_documents({'path': '/status'})
+    count_docs = len(list(nginx_col.find()))
+    count_get = nginx_col.count_documents({'method': 'GET'})
+    count_post = nginx_col.count_documents({'method': 'POST'})
+    count_put = nginx_col.count_documents({'method': 'PUT'})
+    count_patch = nginx_col.count_documents({'method': 'PATCH'})
+    count_delete = nginx_col.count_documents({'method': 'DELETE'})
+
+    count_status_path = nginx_col.count_documents({'path': '/status'})
 
     print('{} logs'.format(count_docs))
-    print('methods:')
+    print('Methods:')
     print('\tmethod GET: {}'.format(count_get))
     print('\tmethod POST: {}'.format(count_post))
     print('\tmethod PUT: {}'.format(count_put))
