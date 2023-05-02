63fb856618137e1a5f82e7f933add9a0ece0f5d7
Gizachew Bayness <eleccrazy24@gmail.com>
2023-05-02 20:11:30 +0300

    School search by topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-"""A python script that interacts with the mongodb database"""
+"""This python modul"e contains code to interact with mongodb"""
 
 
 def schools_by_topic(mongo_collection, topic):
-    """ returns the list of school having a specific topic """
-    docs = mongo_collection.find({"topics": topic})
-    return list(docs)
+    """Returns the list of school having a specific topic"""
+    return [doc for doc in mongo_collection.find({"topics": topic})]
